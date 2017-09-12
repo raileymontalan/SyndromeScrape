ee19825f647ad4f2c0096e68ed8804490e3497fa
Railey <lee_ray@outlook.com>
2017-09-12 15:56:49 +0800

    Improved rss.py

--- Webscraping/rss.py
+++ Webscraping/rss.py
@@ -1,24 +1,6 @@
 import feedparser
  
-# Function to fetch the rss feed and return the parsed RSS
-def parseRSS(rss_url):
-    return feedparser.parse(rss_url) 
-    
-# Function grabs the rss feed headlines (titles) and returns them as a list
-def getHeadlines(rss_url):
-    headlines = []
-    
-    feed = parseRSS(rss_url)
-    for newsitem in feed['items']:
-        headlines.append(newsitem['title'])
-    
-    return headlines
- 
-# A list to hold all headlines
-allheadlines = []
- 
-# List of RSS feeds that we will fetch and combine
-newsurls = {
+feed_urls = {
     'gma-serbisyopubliko':      'http://www.gmanetwork.com/news/rss/serbisyopubliko/',
     'gma-newstv':               'http://www.gmanetwork.com/news/rss/newstv/',
     'gma-photo':                'http://www.gmanetwork.com/news/rss/photo/',
@@ -28,13 +10,16 @@ newsurls = {
     'mb':                       'http://mb.com.ph/mb-feed/',
     'rappler':                  'http://feeds.feedburner.com/rappler/'
 }
- 
-# Iterate over the feed urls
-for key,url in newsurls.items():
-    # Call getHeadlines() and combine the returned headlines with allheadlines
-    allheadlines.extend(getHeadlines(url))
- 
- 
-# Iterate over the allheadlines list and print each headline
-for hl in allheadlines:
-    print(hl)
+
+articles = []
+
+def getArticles(feed_url):
+    feed = feedparser.parse(feed_url)
+    for article in feed['items']:
+        articles.append(article['title'])
+
+for key, url in feed_urls.items():
+    getArticles(url)
+
+for article in articles:
+    print(article)
