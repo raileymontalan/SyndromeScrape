dc2472c26a8954d1931c1ec18588856ea63d36ac
Beatrice Adajar <beatriceadajar@outlook.com>
2018-04-22 18:23:03 +0800

    Fix uploader to Firestore

--- fassster-api/update_firestore.py
+++ fassster-api/update_firestore.py
@@ -2,16 +2,20 @@ import json
 from copy import deepcopy
 from itertools import groupby
 from google.cloud import firestore
-from pymongo import MongoClient
+# from pymongo import MongoClient
 
 # initialize firebase
 db = firestore.Client()
 
 # retrieve data from database
-client = MongoClient()
-incidents = client.news.incidents
-changes = client.news.changes
-statuses = client.news.statuses
+# client = MongoClient()
+# incidents = client.news.incidents
+# changes = client.news.changes
+# statuses = client.news.statuses
+
+incidents = json.load(open('data/incidents5.json'))
+statuses = json.load(open('data/statuses5.json'))
+changes = json.load(open('data/changes5.json'))
 
 # start with incidents
 # segregate each incident by disease
@@ -19,8 +23,8 @@ i_dengue = []
 i_measles = []
 i_typhoid = []
 
-for document in incidents.find():
-    del document['_id']
+for document in incidents:
+    # del document['_id']
     del document['date-end']
     if document['disease'] == 'dengue':
         i_dengue.append(document)
@@ -31,21 +35,63 @@ for document in incidents.find():
     del document['disease']
 
 # group diseases by date
-i_dengue = [list(g) for k, g in groupby(
-    i_dengue, key=lambda x: x['date-start'])]
-i_measles = [list(g) for k, g in groupby(
-    i_measles, key=lambda x: x['date-start'])]
-i_typhoid = [list(g) for k, g in groupby(
-    i_typhoid, key=lambda x: x['date-start'])]
+# i_dengue = [list(g) for k, g in groupby(
+#     i_dengue, key=lambda x: x['date-start'].split('-')[0])]
+# i_measles = [list(g) for k, g in groupby(
+#     i_measles, key=lambda x: x['date-start'].split('-')[0])]
+# i_typhoid = [list(g) for k, g in groupby(
+#     i_typhoid, key=lambda x: x['date-start'].split('-')[0])]
+
+# print(i_dengue)
+
+i_dengue_docs = []
+i_measles_docs = []
+i_typhoid_docs = []
+
+for i in range(2008, 2019):
+    year = []
+
+    for doc in i_dengue:
+        print(doc['date-start'].split('-')[0])
+        print(str(i))
+        
+        if doc['date-start'].split('-')[0] == str(i):
+            year.append(doc)
+    if year:
+        i_dengue_docs.append(year)
+
+for i in range(2008, 2019):
+    year = []
+
+    for doc in i_measles:
+        print(doc['date-start'].split('-')[0])
+        print(str(i))
+        
+        if doc['date-start'].split('-')[0] == str(i):
+            year.append(doc)
+    if year:
+        i_measles_docs.append(year)
+
+for i in range(2008, 2019):
+    year = []
+
+    for doc in i_dengue:
+        print(doc['date-start'].split('-')[0])
+        print(str(i))
+        
+        if doc['date-start'].split('-')[0] == str(i):
+            year.append(doc)
+    if year:
+        i_dengue_docs.append(year)
 
 # format each group appropriately
 # dengue incidents
-for group in i_dengue:
+for group in i_dengue_docs:
     features = []
 
     for doc in group:
         coordinates = deepcopy(doc['code'])
-        del doc['code']
+        
         obj = {
             "type": "Feature",
             "properties": doc,
@@ -74,12 +120,12 @@ for group in i_dengue:
     #         json.dump(js, f, ensure_ascii=False, indent=4)
 
 # measles incidents
-for group in i_measles:
+for group in i_measles_docs:
     features = []
 
     for doc in group:
         coordinates = deepcopy(doc['code'])
-        del doc['code']
+        
         obj = {
             "type": "Feature",
             "properties": doc,
@@ -108,12 +154,12 @@ for group in i_measles:
     #         json.dump(js, f, ensure_ascii=False, indent=4)
 
 # typhoid incidents
-for group in i_typhoid:
+for group in i_typhoid_docs:
     features = []
 
     for doc in group:
         coordinates = deepcopy(doc['code'])
-        del doc['code']
+        
         obj = {
             "type": "Feature",
             "properties": doc,
@@ -147,8 +193,8 @@ c_dengue = []
 c_measles = []
 c_typhoid = []
 
-for document in incidents.find():
-    del document['_id']
+for document in changes:
+    # del document['_id']
     del document['date-end']
     if document['disease'] == 'dengue':
         c_dengue.append(document)
@@ -158,21 +204,62 @@ for document in incidents.find():
         c_typhoid.append(document)
 
 # group diseases by date
-c_dengue = [list(g) for k, g in groupby(
-    c_dengue, key=lambda x: x['date-start'])]
-c_measles = [list(g) for k, g in groupby(
-    c_measles, key=lambda x: x['date-start'])]
-c_typhoid = [list(g) for k, g in groupby(
-    c_typhoid, key=lambda x: x['date-start'])]
+# c_dengue = [list(g) for k, g in groupby(
+#     c_dengue, key=lambda x: x['date-start'])]
+# c_measles = [list(g) for k, g in groupby(
+#     c_measles, key=lambda x: x['date-start'])]
+# c_typhoid = [list(g) for k, g in groupby(
+#     c_typhoid, key=lambda x: x['date-start'])]
+
+c_dengue_docs = []
+c_measles_docs = []
+c_typhoid_docs = []
+
+for i in range(2008, 2019):
+    year = []
+
+    for doc in c_dengue:
+        print(doc['date-start'].split('-')[0])
+        print(str(i))
+
+        if doc['date-start'].split('-')[0] == str(i):
+            year.append(doc)
+    if year:
+        c_dengue_docs.append(year)
+
+for i in range(2008, 2019):
+    year = []
+
+    for doc in c_measles:
+        print(doc['date-start'].split('-')[0])
+        print(str(i))
+
+        if doc['date-start'].split('-')[0] == str(i):
+            year.append(doc)
+    if year:
+        c_measles_docs.append(year)
+
+for i in range(2008, 2019):
+    year = []
+
+    for doc in c_dengue:
+        print(doc['date-start'].split('-')[0])
+        print(str(i))
+
+        if doc['date-start'].split('-')[0] == str(i):
+            year.append(doc)
+    if year:
+        c_dengue_docs.append(year)
 
 # format each group appropriately
 # dengue changes
-for group in c_dengue:
+for group in c_dengue_docs:
     features = []
 
     for doc in group:
+        print(doc)
         coordinates = deepcopy(doc['code'])
-        del doc['code']
+        
         obj = {
             "type": "Feature",
             "properties": doc,
@@ -201,12 +288,12 @@ for group in c_dengue:
     #         json.dump(js, f, ensure_ascii=False, indent=4)
 
 # measles changes
-for group in c_measles:
+for group in c_measles_docs:
     features = []
 
     for doc in group:
         coordinates = deepcopy(doc['code'])
-        del doc['code']
+        
         obj = {
             "type": "Feature",
             "properties": doc,
@@ -226,7 +313,6 @@ for group in c_measles:
         u'features': features
     }, firestore.CreateIfMissingOption(True))
 
-    
     js = {
         'type': 'FeatureCollection',
         'features': features
@@ -236,12 +322,11 @@ for group in c_measles:
     #         json.dump(js, f, ensure_ascii=False, indent=4)
 
 # typhoid changes
-for group in c_typhoid:
+for group in c_typhoid_docs:
     features = []
 
     for doc in group:
         coordinates = deepcopy(doc['code'])
-        del doc['code']
         obj = {
             "type": "Feature",
             "properties": doc,
@@ -266,8 +351,8 @@ for group in c_typhoid:
         'features': features
     }
 
-    # with open('changes-' + doc_id + '.json', 'w', encoding='utf-8') as f:
-    #         json.dump(js, f, ensure_ascii=False, indent=4)
+    with open('changes-' + doc_id + '.json', 'w', encoding='utf-8') as f:
+            json.dump(js, f, ensure_ascii=False, indent=4)
 
 # statuses
 # segregate each status by disease
@@ -275,8 +360,8 @@ s_dengue = []
 s_measles = []
 s_typhoid = []
 
-for document in incidents.find():
-    del document['_id']
+for document in statuses:
+    # del document['_id']
     del document['date-end']
     if document['disease'] == 'dengue':
         s_dengue.append(document)
@@ -286,21 +371,61 @@ for document in incidents.find():
         s_typhoid.append(document)
 
 # group diseases by date
-s_dengue = [list(g) for k, g in groupby(
-    s_dengue, key=lambda x: x['date-start'])]
-s_measles = [list(g) for k, g in groupby(
-    s_measles, key=lambda x: x['date-start'])]
-s_typhoid = [list(g) for k, g in groupby(
-    s_typhoid, key=lambda x: x['date-start'])]
+# s_dengue = [list(g) for k, g in groupby(
+#     s_dengue, key=lambda x: x['date-start'])]
+# s_measles = [list(g) for k, g in groupby(
+#     s_measles, key=lambda x: x['date-start'])]
+# s_typhoid = [list(g) for k, g in groupby(
+#     s_typhoid, key=lambda x: x['date-start'])]
+
+s_dengue_docs = []
+s_measles_docs = []
+s_typhoid_docs = []
+
+for i in range(2008, 2019):
+    year = []
+
+    for doc in s_dengue:
+        print(doc['date-start'].split('-')[0])
+        print(str(i))
+
+        if doc['date-start'].split('-')[0] == str(i):
+            year.append(doc)
+    if year:
+        s_dengue_docs.append(year)
+
+for i in range(2008, 2019):
+    year = []
+
+    for doc in s_measles:
+        print(doc['date-start'].split('-')[0])
+        print(str(i))
+
+        if doc['date-start'].split('-')[0] == str(i):
+            year.append(doc)
+    if year:
+        s_measles_docs.append(year)
+
+for i in range(2008, 2019):
+    year = []
+
+    for doc in s_dengue:
+        print(doc['date-start'].split('-')[0])
+        print(str(i))
+
+        if doc['date-start'].split('-')[0] == str(i):
+            year.append(doc)
+    if year:
+        s_dengue_docs.append(year)
 
 # format each group appropriately
 # dengue statuses
-for group in s_dengue:
+for group in s_dengue_docs:
     features = []
 
     for doc in group:
         coordinates = deepcopy(doc['code'])
-        del doc['code']
+        
         obj = {
             "type": "Feature",
             "properties": doc,
@@ -329,12 +454,12 @@ for group in s_dengue:
     #         json.dump(js, f, ensure_ascii=False, indent=4)
 
 # measles statuses
-for group in s_measles:
+for group in s_measles_docs:
     features = []
 
     for doc in group:
         coordinates = deepcopy(doc['code'])
-        del doc['code']
+        
         obj = {
             "type": "Feature",
             "properties": doc,
@@ -363,12 +488,12 @@ for group in s_measles:
     #         json.dump(js, f, ensure_ascii=False, indent=4)
 
 # typhoid statuses
-for group in s_typhoid:
+for group in s_typhoid_docs:
     features = []
 
     for doc in group:
         coordinates = deepcopy(doc['code'])
-        del doc['code']
+        
         obj = {
             "type": "Feature",
             "properties": doc,
